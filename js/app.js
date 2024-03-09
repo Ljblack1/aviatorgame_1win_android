91b11a14c1930d7a1d817afdce901be6cc57e387
Ljblack1 <152862088+Ljblack1@users.noreply.github.com>
2024-03-09 06:35:45 -0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ balanceBtn.addEventListener("click", () => {
   balanceContainer.style.display = "none";
 })
 
-let mainBalance = 64278.91;
+let mainBalance = 29278.91;
 
 balance.textContent = mainBalance;
 
@@ -94,7 +94,7 @@ btn.addEventListener('click', () => {
         displayAlert(cashout);
         // setBackToDefault();
       })
-    }, 4600)
+    }, 3000)
 
 
   }, 3000)
@@ -131,7 +131,7 @@ btnTwo.addEventListener('click', () => {
         displayAlertTwo(cashout);
         // setBackToDefault();
       })
-    }, 4600)
+    }, 3000)
 
   }, 2500)
 })
