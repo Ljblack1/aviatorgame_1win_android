75ca93b9b6b91a0be9a1835fe8eeb9dea8b5a894
Ljblack1 <152862088+Ljblack1@users.noreply.github.com>
2024-07-31 11:06:53 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ balance.addEventListener("click", () => {
   balanceContainer.style.display = "flex";
 })
 
-const mainCurrency = "GHS";
+const mainCurrency = "USD";
 
 document.addEventListener("DOMContentLoaded", () => {
   currencys.forEach((currency) => {
@@ -70,7 +70,7 @@ balanceBtn.addEventListener("click", () => {
   balanceContainer.style.display = "none";
 })
 
-let mainBalance = 7913.81;
+let mainBalance = 2900.91;
 
 balance.textContent = mainBalance;
 
