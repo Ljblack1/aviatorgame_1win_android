4eb75196358abd399269600d86f38b2e2e3414ab
Ljblack1 <152862088+Ljblack1@users.noreply.github.com>
2025-02-19 06:56:19 -0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -70,7 +70,7 @@ balanceBtn.addEventListener("click", () => {
   balanceContainer.style.display = "none";
 })
 
-let mainBalance = 13741.92;
+let mainBalance = 22741.78;
 
 balance.textContent = mainBalance;
 
