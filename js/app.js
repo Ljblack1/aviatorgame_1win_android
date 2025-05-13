116115edc6fbc83eeefb152a0ae97ad606de6aad
Ljblack1 <152862088+Ljblack1@users.noreply.github.com>
2025-05-13 07:37:23 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -70,7 +70,7 @@ balanceBtn.addEventListener("click", () => {
   balanceContainer.style.display = "none";
 })
 
-let mainBalance = 58987.11;
+let mainBalance = 4851.41;
 
 balance.textContent = mainBalance;
 
