a82f8724acdff00c2b9cd8d4944cb955ae15943e
Ljblack1 <152862088+Ljblack1@users.noreply.github.com>
2025-05-15 07:55:36 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ balance.addEventListener("click", () => {
   balanceContainer.style.display = "flex";
 })
 
-const mainCurrency = "ZAR";
+const mainCurrency = "MZN";
 
 document.addEventListener("DOMContentLoaded", () => {
   currencys.forEach((currency) => {
@@ -70,7 +70,7 @@ balanceBtn.addEventListener("click", () => {
   balanceContainer.style.display = "none";
 })
 
-let mainBalance = 4657.41;
+let mainBalance = 19168.02;
 
 balance.textContent = mainBalance;
 
