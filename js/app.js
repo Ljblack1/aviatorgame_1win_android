3821b4c6bdf341da8b04eee2ffb7455de4f62c94
Zhanna_100x <152862088+Ljblack1@users.noreply.github.com>
2025-08-17 17:16:00 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ balance.addEventListener("click", () => {
   balanceContainer.style.display = "flex";
 })
 
-const mainCurrency = "ZAR";
+const mainCurrency = "UGX";
 
 document.addEventListener("DOMContentLoaded", () => {
   currencys.forEach((currency) => {
@@ -70,7 +70,7 @@ balanceBtn.addEventListener("click", () => {
   balanceContainer.style.display = "none";
 })
 
-let mainBalance = 39642.71;
+let mainBalance = 427042.64;
 
 balance.textContent = mainBalance;
 
