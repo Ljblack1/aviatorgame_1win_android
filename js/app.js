db3d78ee7ca059ff5bbcc671be21057f3f0b96c9
Zhanna_100x <152862088+Ljblack1@users.noreply.github.com>
2025-09-20 07:11:14 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ balance.addEventListener("click", () => {
   balanceContainer.style.display = "flex";
 })
 
-const mainCurrency = "ZAR";
+const mainCurrency = "NGN";
 
 document.addEventListener("DOMContentLoaded", () => {
   currencys.forEach((currency) => {
@@ -70,7 +70,7 @@ balanceBtn.addEventListener("click", () => {
   balanceContainer.style.display = "none";
 })
 
-let mainBalance = 31547.61;
+let mainBalance = 75547.11;
 
 balance.textContent = mainBalance;
 
@@ -535,3 +535,4 @@ function displayAlertTwo(cashout) {
 function setBackToDefault() {
   btnAmount.textContent = 0;
 }
+
